7df16894d4c350b4747cde6b3e5dab7cbc3a0607
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-04-12 14:37:03 +0200

    remove dependency on the Controller trait

--- src/main/scala/uk/gov/hmrc/play/fsm/JourneyController.scala
+++ src/main/scala/uk/gov/hmrc/play/fsm/JourneyController.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.hmrc.play.fsm
 
 import play.api.data.Form
@@ -38,7 +22,6 @@ import scala.concurrent.{ExecutionContext, Future}
   *   - authorisedShowCurrentStateWhen
   */
 trait JourneyController {
-  self: Controller =>
 
   /** This has to be injected in the concrete controller */
   val journeyService: JourneyService
@@ -67,7 +50,7 @@ trait JourneyController {
 
   /** redirects to the endpoint matching state */
   final val redirect: RouteFactory =
-    (state: StateAndBreadcrumbs) => (request: Request[_]) => Redirect(getCallFor(state._1)(request))
+    (state: StateAndBreadcrumbs) => (request: Request[_]) => Results.Redirect(getCallFor(state._1)(request))
 
   /** applies transition to the current state */
   def apply(
@@ -127,7 +110,8 @@ trait JourneyController {
           journeyService.currentState.flatMap {
             case Some((state, _)) =>
               Future.successful(
-                Redirect(getCallFor(state))
+                Results
+                  .Redirect(getCallFor(state))
                   .flashing(Flash {
                     val data = formWithErrors.data
                     if (data.isEmpty) Map("dummy" -> "") else data
